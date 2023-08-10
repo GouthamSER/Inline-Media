cc654939b101247d52a8398efcb5c801130e53e1
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-08-10 18:22:09 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,17 @@
+class script(object):
+  START_TXT="""**Hi, I'm Media Search bot**
+
+Here you can search files in inline mode. Just press following buttons and start searching.
+"""
+  HELP_TXT="""Hᴇʏ {}
+Tʜɪs ɪs ᴛʜᴇ Hᴇʟᴘ Txᴛ..
+/add - to use index the files from channel (admin)
+/stats - to see how many files are in db
+/log - to see the errors
+/channel - to see how many channels connected
+"""
+  ABOUT_TXT="""Hᴇʏ {}
+Nᴀᴍᴇ - Kᴜᴛᴛᴜ Bᴏᴛ 2
+DB - Mongodb
+Server - Render
+"""
