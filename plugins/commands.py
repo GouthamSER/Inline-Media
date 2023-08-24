5fd4a4c0cd606a8c81bbd3d310732e9ad9788e31
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-08-24 17:41:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -85,7 +85,7 @@ async def startmes(bot:Client, mes:CallbackQuery):
                 ]]
             ))
     elif mes.data=="stats":
-        msg = await message.edit_text("𝐴𝑐𝑐𝑒𝑠𝑠𝑖𝑛𝑔 𝑆𝑡𝑎𝑡𝑢𝑠 𝐷𝑎𝑡𝑎 ✔..")
+        msg = await mes.edit_text("𝐴𝑐𝑐𝑒𝑠𝑠𝑖𝑛𝑔 𝑆𝑡𝑎𝑡𝑢𝑠 𝐷𝑎𝑡𝑎 ✔..")
         asyncio.sleep(1)
         await msg.edit("𝐴𝑐𝑐𝑒𝑠𝑠𝑖𝑛𝑔 𝑆𝑡𝑎𝑡𝑢𝑠 𝐷𝑎𝑡𝑎 ✔✔.")
         asyncio.sleep(1)
