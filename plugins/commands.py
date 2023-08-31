fbef8e066075ed088b67fe03ddfdcab54927d2d2
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-08-31 12:02:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -34,7 +34,7 @@ async def start(bot, message):
             )
             return
         
-        if not await db.is_user_exist(message.from_user.id):
+    if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOGP_TXT.format(message.from_user.id, message.from_user.mention))
         
