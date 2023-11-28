dffebb6a0888a7ff45a50d90129fcd3b2f32ba0d
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-11-28 20:02:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,6 +29,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
+AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 PICS=environ.get('PICS', 'PhoTO Link here')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
