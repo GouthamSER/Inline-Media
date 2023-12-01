f5302040d4f0922c558c3aaa81ae051699ac5d48
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-12-01 19:23:46 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -27,14 +27,7 @@ async def filter(bot, message):
                  )
             )
             return
-        except Exception:
-            await bot.send_message(
-                chat_id=message.from_user.id,
-                text="Something went Wrong.",
-                parse_mode="markdown",
-                disable_web_page_preview=True
-            )
-        return
+    
     if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
         return
     if 2 < len(message.text) < 100:    
