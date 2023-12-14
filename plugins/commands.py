801b20928f1fb31094b0fbfc6cf1df81467832ca
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-12-14 12:39:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,6 +23,7 @@ async def start(bot, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await bot.send_message(LOG_CHANNEL, script.LOGP_TXT.format(message.from_user.id, message.from_user.mention))
+        return
     user_cmnd = message.text
     if user_cmnd.startswith("/start kuttu"):
         
