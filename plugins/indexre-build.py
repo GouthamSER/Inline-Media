e0ba0af874e7e5e2d2c2b87a8def978c91eb4c25
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-10-30 22:55:07 +0530

    Update indexre-build.py

--- plugins/indexre-build.py
+++ plugins/indexre-build.py
@@ -169,7 +169,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                 if not media:
                     unsupported += 1
                     continue
-                media.file_type = message.media.value
+                media.file_type = file_type
                 media.caption = message.caption
                 aynav, vnay = await save_file(media)
                 if aynav:
