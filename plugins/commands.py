ea151cb51c730d9ecf192b25575664a7620d7be4
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-11-24 15:06:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,6 +19,10 @@ FORCE_SUB2 = "wudixh"    # Replace with your actual channel username (without @)
 @Client.on_message(filters.command("start"))
 async def start(bot, message):
     """Handles the /start command and prompts for channel subscription if necessary."""
+        # USER SAVING IN DB
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOGP_TXT.format(message.from_user.id, message.from_user.mention))
     user_cmd = message.text
 
     if user_cmd.startswith("/start kuttu"):
